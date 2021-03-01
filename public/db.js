097d6e076cacb1bf132eeceb96630a701df397ee
Efrain Nevarez <efrain.nevarezjr@gmail.com>
2021-02-28 18:57:31 -0600

    edited db.js

---
+++ public/db.js
@@ -0,0 +1,8 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+let db;
+const request = indexedDB.open("budget", 1);
